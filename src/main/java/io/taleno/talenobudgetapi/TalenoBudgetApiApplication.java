3e70af2a357311c744f3010e9b77be464c98022a
Aden Moukouyou <moukouyou@gmail.com>
2023-10-25 18:49:29 -0400

    initial commit

---
+++ src/main/java/io/taleno/talenobudgetapi/TalenoBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package io.taleno.talenobudgetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalenoBudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalenoBudgetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/taleno/talenobudgetapi/TalenoBudgetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.taleno.talenobudgetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalenoBudgetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
