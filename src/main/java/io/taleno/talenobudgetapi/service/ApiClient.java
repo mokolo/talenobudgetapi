cadef0bf6f47d25caf832547d5f6d45e3af34813
Aden Moukouyou <moukouyou@gmail.com>
2023-10-27 12:19:05 -0400

    integration Spring Feign pour référencer automatiquement les urls des  autres microservices grâce à leur noms plutôt que grâce à leurs url et port, en passant par eureka

--- src/main/java/io/taleno/talenobudgetapi/service/ApiClient.java
+++ src/main/java/io/taleno/talenobudgetapi/service/ApiClient.java
@@ -6,7 +6,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url="localhost:8081/", value="USER-SERVICE")
+//@FeignClient(url="localhost:8081/", value="USER-SERVICE")
+/**
+ * en choisissant d'utilisant l'attribut name, plutôt que le couple (url et value),
+ *  le service va récupérer, dynamiquement les urls auprès d'Eureka. Ce qui, permettra de mettre en oeuvre le
+ *  load balancing.
+ * */
+@FeignClient(name="USER-SERVICE")
 public interface ApiClient {
 
     @GetMapping("api/users/{id}")
